ff2af4ba4f2c2b0e9848df4154c6340a63972e7b
Igor Rafael <igorrafaelreis00@gmail.com>
2026-07-25 01:29:43 -0400

    feat: integrando front-end com back-end para Deposito e Saque

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,4 @@
 const urlAPI = "http://127.0.0.1:8000";
-
 let usuarioLogado = "";
 
 async function buscarSaldo() {
@@ -32,3 +31,85 @@ async function buscarSaldo() {
 }
 
 document.getElementById("btn-acessar").addEventListener("click", buscarSaldo);
+
+
+
+async function realizarDeposito() {
+    const inputValor = document.getElementById("valor");
+    const valor = parseFloat(inputValor.value);
+
+    if (isNaN(valor) || valor <= 0) {
+        alert("Digite um valor válido para depósito.");
+        return;
+    }
+    
+    if (!usuarioLogado) {
+        alert("Identifique-se primeiro digitando seu nome de usuário.");
+        return;
+    }
+
+    try {
+        const resposta = await fetch(`${urlAPI}/deposito/${usuarioLogado}/${valor}`, {
+            method: "POST"
+        });
+
+        const dados = await resposta.json();
+
+        if (dados.status === "sucesso") {
+            document.getElementById("saldo").innerText = `R$ ${dados.novo_saldo.toFixed(2)}`;
+
+
+            inputValor.value = "";
+
+            alert("Depósito realizado com sucesso!");
+        } else {
+            alert(dados.motivo || "Erro ao realizar depósito.");
+        }
+
+    } catch (erro) {
+        console.error("Erro no depósito:", erro);
+        alert("Erro ao conectar com o servidor.");
+    }
+}
+
+document.getElementById("btn-depositar").addEventListener("click", realizarDeposito);
+    
+
+async function realizarSaque() {
+    const inputValor = document.getElementById("valor");
+    const valor = parseFloat(inputValor.value);
+    const senhaDigitada = document.getElementById("input-senha").value;
+
+    if (isNaN(valor) || valor <= 0) {
+        alert("Digite um valor válido para saque");
+        return;
+    }
+
+    if (!usuarioLogado) {
+        alert("Identifique-se primeiro digitando seu nome de usuário.");
+        return;
+    }
+
+    try {
+        const resposta = await fetch(`${urlAPI}/saque/${usuarioLogado}/${senhaDigitada}/${valor}`, {
+            method: "POST"
+        });
+
+        const dados = await resposta.json();
+
+        if (dados.status === "sucesso") {
+            document.getElementById("saldo").innerText = `R$ ${dados.novo_saldo.toFixed(2)}`;
+
+            inputValor.value = "";
+            alert("Saque realizado com sucesso!");
+        } else {
+            alert(dados.motivo || "Erro ao realizar saque.");
+        }
+        
+    } catch (erro) {
+        console.error("Erro no saque:", erro);
+        alert("Erro ao conectar com o servidor.");
+    }
+}
+
+document.getElementById("btn-sacar").addEventListener("click", realizarSaque);
